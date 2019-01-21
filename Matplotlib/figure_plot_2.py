a28cf02b0ad4baa8312045458cc92f7ec4ced6f3
Ptr <krolak.piotr.lca@gmail.com>
2019-01-21 21:55:09 +0100

    figure_plot_2.py dodanie opisow

--- Matplotlib/figure_plot_2.py
+++ Matplotlib/figure_plot_2.py
@@ -1,4 +1,4 @@
-# documentation part 1
+# documentation part 2
 # Parts of a Figure
 
 import matplotlib.pyplot as plt
@@ -6,15 +6,16 @@ import numpy as np
 
 x = np.linspace(0, 2, 100)
 
+# plt.plot(x, y, label='opis w legendzie wykresu')
 plt.plot(x, x, label='linear')
 plt.plot(x, x**2, label='quadratic')
 plt.plot(x, x**3, label='cubic')
 
-plt.xlabel('x label')
-plt.ylabel('y label')
+plt.xlabel('x label') # opis osi x
+plt.ylabel('y label') # opis osi y 
 
-plt.title("Simple Plot")
+plt.title("Simple Plot") # tytul wykresu
 
-plt.legend()
+plt.legend() # wywolanie legendy wykresu
 
-plt.show()
+plt.show() # wywolanie wykresu
