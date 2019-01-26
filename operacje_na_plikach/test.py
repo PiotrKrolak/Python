9e0f365e26af1effb81adf5d93c3f3073b3885fe
Ptr <krolak.piotr.lca@gmail.com>
2019-01-26 01:20:46 +0100

    ~ dodane zrzucanie lini do tablicy(listy)

--- operacje_na_plikach/test.py
+++ operacje_na_plikach/test.py
@@ -1,4 +1,21 @@
+import csv
+
+txt = "data.txt"
+csv = "data.csv"
+
 #operacje na plikach tekstowych
 
-tekst = open('tekst.txt').read()
-print(tekst)
+tekst = open(csv).read()
+#print(tekst)
+
+linie = [row for row in open(csv)] # wpisuje do listy pojedyncze linie z pliku
+print(linie[0]) #wypisuje pojedyncza linie czytanego dokumentu
+
+
+#operacja na csvfile
+"""
+with open(tekst, 'r') as csvfile:
+    # deklarujemy nasz *czytacz*
+    # parametr *delimiter* jest opcjonalny i wskazuje jaki został w pliku użyty separator
+    csvreader = csv.reader(csvfile, delimiter=',')
+"""
